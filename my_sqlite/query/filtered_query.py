4aedeaad2a5d5f0e72f1240d1fba9f5978be121b
Ian Beauregard <ian.beauregard@gmail.com>
2021-04-27 10:44:48 -0400

    Default value for a FilteredQuery filter (True)

--- my_sqlite/query/filtered_query.py
+++ my_sqlite/query/filtered_query.py
@@ -6,7 +6,7 @@ class FilteredQuery(AbstractQuery):
     @abstractmethod
     def __init__(self):
         super().__init__()
-        self._filter = None
+        self._filter = True
 
     def where(self, column_name, condition):
         self._filter = lambda entry: condition(entry[column_name])
