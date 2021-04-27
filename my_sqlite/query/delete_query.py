dc7f0c80ef9de9ff5caa7798eb010ee24e9c1fa7
Ian Beauregard <ian.beauregard@gmail.com>
2021-04-27 15:59:02 -0400

    Add underscore to private method

--- my_sqlite/query/delete_query.py
+++ my_sqlite/query/delete_query.py
@@ -13,13 +13,13 @@ class DeleteQuery(FilteredQuery):
 
     def run(self):
         with open(self._main_table) as table:
-            column_headers = self.strip_and_split(next(table))
-            entries = (dict(zip(column_headers, entry)) for entry in map(self.strip_and_split, table))
+            column_headers = self._strip_and_split(next(table))
+            entries = (dict(zip(column_headers, entry)) for entry in map(self._strip_and_split, table))
             non_deleted_entries = [entry.values() for entry in entries if not self._filter(entry)]
         with open(self._main_table, 'w') as table:
             table.write(f"{self._sep.join(column_headers)}{self._linesep}"
                         f"{os.linesep.join(self._sep.join(entry) for entry in non_deleted_entries)}{self._linesep}")
 
     @classmethod
-    def strip_and_split(cls, line):
+    def _strip_and_split(cls, line):
         return line.strip().split(cls._sep)
