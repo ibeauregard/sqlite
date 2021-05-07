3d17a3304e89819191f1314acd99325f873c3295
Ian Beauregard <ian.beauregard@gmail.com>
2021-05-07 14:00:11 -0400

    Capitalize test queries

--- test/builder.py
+++ test/builder.py
@@ -14,84 +14,84 @@ class TestSuite:
 
 
 if __name__ == '__main__':
-    select_queries = [('select * from players join batting on a,b,c', 'Syntax error'),
+    select_queries = [('SELECT * FROM players JOIN batting ON a,b,c', 'Syntax error'),
 
-                      ('select * from players where id=42', 'Syntax error'),
+                      ('SELECT * FROM players WHERE id=42', 'Syntax error'),
 
-                      ('select * from players where id=42=24', 'Syntax error'),
+                      ('SELECT * FROM players WHERE id=42=24', 'Syntax error'),
 
-                      ('select * from players where id', 'Syntax error'),
+                      ('SELECT * FROM players WHERE id', 'Syntax error'),
 
-                      ('select * from players join batting, pitching', 'Syntax error'),
+                      ('SELECT * FROM players JOIN batting, pitching', 'Syntax error'),
 
-                      ('select * from players order by id ???', 'Syntax error'),
+                      ('SELECT * FROM players ORDER BY id ???', 'Syntax error'),
 
-                      ('select * from players order by id namelast', 'Syntax error'),
+                      ('SELECT * FROM players ORDER BY id nameLast', 'Syntax error'),
 
-                      ('select * from players limit ten', 'Syntax error'),
+                      ('SELECT * FROM players LIMIT ten', 'Syntax error'),
 
                       ('SELECT FROM players', 'Syntax error'),
 
                       ('SELECT * FROM players LIMIT 10', 'Success'),
 
-                      ('SELECT namelast, namefirst FROM players limit 10', 'Success'),
+                      ('SELECT nameLast, nameFirst FROM players LIMIT 10', 'Success'),
 
-                      ('SELECT players.namelast, players.namefirst from players limit 10', 'Success'),
+                      ('SELECT players.nameLast, players.nameFirst FROM players LIMIT 10', 'Success'),
 
                       ('SELECT * FROM players WHERE id="0"', 'Success'),
 
-                      ('SELECT namelast, namefirst, yearid, hr '
-                       'FROM players join batting on batting.playerid = players.id '
-                       'order by hr desc, namelast, namefirst, yearid limit 10', 'Success')]
+                      ('SELECT nameLast, nameFirst, yearId, hr '
+                       'FROM players JOIN batting ON batting.playerId = players.id '
+                       'ORDER BY hr DESC, nameLast, nameFirst, yearId LIMIT 10', 'Success')]
 
-    update_queries = [('update players', 'Syntax error'),
+    update_queries = [('UPDATE players', 'Syntax error'),
 
-                      ('update players, batting set id = "42"', 'Syntax error'),
+                      ('UPDATE players, batting SET id = "42"', 'Syntax error'),
 
-                      ('update players set a = b', 'Syntax error'),
+                      ('UPDATE players SET a = b', 'Syntax error'),
 
-                      ('update players set id = 1000', 'Syntax error'),
+                      ('UPDATE players SET id = 1000', 'Syntax error'),
 
-                      ('update players set id = "42", nameGiven: "Roger"', 'Syntax error'),
+                      ('UPDATE players SET id = "42", nameGiven: "Roger"', 'Syntax error'),
 
-                      ('update players set (birthcountry = "USofA", deathcountry = "Canada") '
-                       'where birthcountry = "USA"', 'Syntax error'),
+                      ('UPDATE players SET (birthCountry = "USofA", deathCountry = "Canada") '
+                       'WHERE birthCountry = "USA"', 'Syntax error'),
 
-                      ('update players set id = "42000" where id = "42"', 'Success'),
+                      ('UPDATE players SET id = "42000" WHERE id = "42"', 'Success'),
 
-                      ('update players set birthcountry = "USofA", deathcountry = "Canada" '
-                       'where birthcountry = "USA"', 'Success')]
+                      ('UPDATE players SET birthCountry = "USofA", deathCountry = "Canada" '
+                       'WHERE birthCountry = "USA"', 'Success')]
 
-    insert_queries = [('insert into players', 'Syntax error'),
+    insert_queries = [('INSERT INTO players', 'Syntax error'),
 
-                      ('insert into players, players (id) values ("100")', 'Syntax error'),
+                      ('INSERT INTO players, players (id) VALUES ("100")', 'Syntax error'),
 
-                      ('insert into players (id, namelast, namefirst) values a, b, c', 'Syntax error'),
+                      ('INSERT INTO players (id, nameLast, nameFirst) VALUES a, b, c', 'Syntax error'),
 
-                      ('insert into players (id, namelast, namefirst) values (a, b, c), (d, e, f)', 'Syntax error'),
+                      ('INSERT INTO players (id, nameLast, nameFirst) VALUES (a, b, c), (d, e, f)', 'Syntax error'),
 
-                      ('insert into players (id, namelast, namefirst) values (500, Roger, Clemens)', 'Syntax error'),
+                      ('INSERT INTO players (id, nameLast, nameFirst) VALUES (500, Roger, Clemens)', 'Syntax error'),
 
-                      ('insert into players (id, namelast, namefirst) '
-                       'values ("999", "Roger", "Clemens"), ("1000", "Roger")', 'Insert error'),
+                      ('INSERT INTO players (id, nameLast, nameFirst) '
+                       'VALUES ("999", "Roger", "Clemens"), ("1000", "Roger")', 'Insert error'),
 
-                      ('insert into players (id) values ("999", "Roger"), ("1000", "Roger")', 'Insert error'),
+                      ('INSERT INTO players (id) VALUES ("999", "Roger"), ("1000", "Roger")', 'Insert error'),
 
-                      ('insert into players (id) values ("9991")', 'Success'),
+                      ('INSERT INTO players (id) VALUES ("9991")', 'Success'),
 
-                      ('insert into players (id) values ("9992"), ("9993")', 'Success'),
+                      ('INSERT INTO players (id) VALUES ("9992"), ("9993")', 'Success'),
 
-                      ('insert into players (id, namelast, namefirst) '
-                       'values ("9994", "Roger", "Clemens")', 'Success'),
+                      ('INSERT INTO players (id, nameLast, nameFirst) '
+                       'VALUES ("9994", "Roger", "Clemens")', 'Success'),
 
-                      ('insert into players (id, namelast, namefirst) '
-                       'values ("9995", "Roger", "Clemens"), ("9996", "Jackie", "Robinson")', 'Success'),
+                      ('INSERT INTO players (id, nameLast, nameFirst) '
+                       'VALUES ("9995", "Roger", "Clemens"), ("9996", "Jackie", "Robinson")', 'Success'),
 
-                      ('insert into players values ("9997", "", "", "", "", "", "", "", '
+                      ('INSERT INTO players VALUES ("9997", "", "", "", "", "", "", "", '
                        '"", "", "", "", "", "", "", '
                        '"", "", "", "", "", "", "")', 'Success'),
 
-                      ('insert into players values ("9998", "", "", "", "", "", "", "", '
+                      ('INSERT INTO players VALUES ("9998", "", "", "", "", "", "", "", '
                        r'"", "", "", "You can \"also\" insert quotes", "", "", "", '
                        '"", "", "", "", "", "", ""),'
                        ''
@@ -99,19 +99,19 @@ if __name__ == '__main__':
                        '"", "", "", "and control characters, such as\n the line feed", "", "", "", '
                        '"", "", "", "", "", "", "")', 'Success')]
 
-    delete_queries = [('delete players where id=42', 'Syntax error'),
+    delete_queries = [('DELETE players WHERE id=42', 'Syntax error'),
 
-                      ('delete players', 'Syntax error'),
+                      ('DELETE players', 'Syntax error'),
 
-                      ('delete from players where id=42', 'Syntax error'),
+                      ('DELETE FROM players WHERE id=42', 'Syntax error'),
 
-                      ('delete from players where id=42=24', 'Syntax error'),
+                      ('DELETE FROM players WHERE id=42=24', 'Syntax error'),
 
-                      ('delete from players where id', 'Syntax error'),
+                      ('DELETE FROM players WHERE id', 'Syntax error'),
 
-                      ('delete from players where id = "919"', 'Success'),
+                      ('DELETE FROM players WHERE id = "919"', 'Success'),
 
-                      ('delete from players', 'Success')]
+                      ('DELETE FROM players', 'Success')]
 
     shutil.copy2('mlb/players.csv', 'mlb/players.csv.backup')
     try:
